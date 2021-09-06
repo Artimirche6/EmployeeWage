6d2d709125e5aebc08f8f16b1c6f7d87b9f875b4
Artimirche6 <artimirche6@gmail.com>
2021-09-06 18:13:43 +0530

    [arti] Added: Emplyoee Attendance Check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,16 @@
 #! /bin/bash -x
 
 echo " Welcome to Employee Wage Attendance System "
+
+#constant variable
+IS_PRESENT=1
+
+#variable
+empCheck=$((RANDOM%2))
+
+if [ $empCheck -eq $IS_PRESENT ]
+then 
+	echo " Employee is Present "
+else 
+	echo " Employee is Absent "
+fi

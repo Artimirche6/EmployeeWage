29dc97fa5c8cd2cf40d4039ca96e490b719ddc17
Artimirche6 <artimirche6@gmail.com>
2021-09-12 01:53:04 +0530

    [arti] Added: Daily Employee Wage using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,42 +4,21 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
-MAX_WORKING_DAYS=20
-MAX_WORK_HR=50
-day=1
 
-totalWorkHr=0
+#variable
+empCheck=$((RANDOM%3)) # 1, 2, 0
 
-function getWorkHr()
-{
-        local empCheck=$1
-        local empHr=0
+case $empCheck in
+	$IS_PRESENT_FULL_TIME)
+		empHr=8
+	;;
+	$IS_PRESENT_PART_TIME)
+		empHr=4
+	;;
+	*)
+		salary=0
+	;;
 
-        case $empCheck in
-                $IS_PRESENT_FULL_TIME)
-                        empHr=8
-                ;;
-                $IS_PRESENT_PART_TIME)
-                        empHr=4
-                ;;
-                *)
-                        salary=0
-                ;;
+esac
 
-        esac
-        echo $empHr
-}
-
-while [ $day -lt $MAX_WORKING_DAYS -a $totalWorkHr -lt $MAX_WORK_HR ]
-do
-        #variable
-        empCheck=$((RANDOM%3))
-
-        empHr=$( getWorkHr $empCheck )
-
-        ((day++))
-        totalWorkHr=$(( totalWorkHr + empHr ))
-        salary=$(( empHr * EMP_RATE_PER_HR ))
-
-done
-
+salary=$(( empHr * EMP_RATE_PER_HR ))

a57ca68ae2b0aaf931a3efd42fc1e89c624178a7
Artimirche6 <artimirche6@gmail.com>
2021-09-12 02:04:42 +0530

    [arti] Added: Function to get Work Hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,29 +5,38 @@ IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
 MAX_WORKING_DAYS=20
-MAX_WORK_HR=100
+MAX_WORK_HR=50
 day=1
 
 totalWorkHr=0
+
+function getWorkHr()
+{
+	local empCheck=$1
+	local empHr=0
+
+	case $empCheck in
+                $IS_PRESENT_FULL_TIME)
+                        empHr=8
+                ;;
+                $IS_PRESENT_PART_TIME)
+                        empHr=4
+                ;;
+                *)
+                        salary=0
+                ;;
+
+        esac
+	echo $empHr
+}
 
 while [ $day -lt $MAX_WORKING_DAYS -a $totalWorkHr -lt $MAX_WORK_HR ]
 do
 	#variable
 	empCheck=$((RANDOM%3))
 
-	case $empCheck in
-		$IS_PRESENT_FULL_TIME)
-			empHr=8
-		;;
-		$IS_PRESENT_PART_TIME)
-			empHr=4
-		;;
-		*)
-			salary=0
-		;;
+	empHr=$( getWorkHr $empCheck )
 
-	esac
-	
 	((day++))
 	totalWorkHr=$(( totalWorkHr + empHr ))
 	salary=$(( empHr * EMP_RATE_PER_HR ))

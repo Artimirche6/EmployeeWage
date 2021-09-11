41508f45e4549147317d084056df83bc154c4792
Artimirche6 <artimirche6@gmail.com>
2021-09-12 01:56:45 +0530

    [arti] Added: Calculation Wages for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,21 +4,31 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
+MAX_WORKING_DAYS=20
 
-#variable
-empCheck=$((RANDOM%3)) # 1, 2, 0
+totalWorkHr=0
 
-case $empCheck in
-	$IS_PRESENT_FULL_TIME)
-		empHr=8
-	;;
-	$IS_PRESENT_PART_TIME)
-		empHr=4
-	;;
-	*)
-		salary=0
-	;;
+for ((day=1;day<=$MAX_WORKING_DAYS;day++))
+do
+	#variable
+	empCheck=$((RANDOM%3))
 
-esac
+	case $empCheck in
+		$IS_PRESENT_FULL_TIME)
+			empHr=8
+		;;
+		$IS_PRESENT_PART_TIME)
+			empHr=4
+		;;
+		*)
+			salary=0
+		;;
 
-salary=$(( empHr * EMP_RATE_PER_HR ))
+	esac
+	
+	totalWorkHr=$(( totalWorkHr + empHr ))
+	salary=$(( empHr * EMP_RATE_PER_HR ))
+
+done
+
+totalSalary=$(( totalWorkHr * EMP_RATE_PER_HR ))
